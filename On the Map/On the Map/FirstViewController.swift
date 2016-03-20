3b3cf8318ae59a62613de634e1c0674a41bba1f7
Ivan Magda <imagda15@gmail.com>
2016-03-20 16:31:23 +0900

    [On the Map] Initial commit.

---
+++ On the Map/On the Map/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  On the Map
+//
+//  Created by Ivan Magda on 20.03.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ On the Map/On the Map/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  On the Map
+//
+//  Created by Ivan Magda on 20.03.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
