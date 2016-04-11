f359f2359431d9f938ce028e76c0848595f635e3
Ivan Magda <imagda15@gmail.com>
2016-04-11 22:06:35 +0900

    [MemeMe] Initial commit

---
+++ P2. UIKit Fundamentals/MemeMe/MemeMe/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  MemeMe
+//
+//  Created by Ivan Magda on 11.04.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+//---------------------------------------------------------
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+//---------------------------------------------------------
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    //-----------------------------------------------------
+    // MARK: Properties
+    //-----------------------------------------------------
+
+    var window: UIWindow?
+
+    //-----------------------------------------------------
+    // MARK: UIApplicationDelegate
+    //-----------------------------------------------------
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ P2. UIKit Fundamentals/MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Ivan Magda on 11.04.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+//----------------------------------------------------
+// MARK: - ViewController: UIViewController
+//----------------------------------------------------
+
+class ViewController: UIViewController {
+    
+    //------------------------------------------------
+    // MARK: View Life Cycle
+    //------------------------------------------------
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
